224764a67898f67eb5a7463522a9293d5e6529f2
Samuel Barbosa Morales <zzzdy11@gmail.com>
2026-05-12 17:02:31 -0500

    archivos cpp

---
+++ src/jugador.cpp
@@ -0,0 +1,19 @@
+#include "jugador.h"
+
+jugador::jugador() {
+    x=1;
+    y=1;
+}
+
+int jugador::getX() {
+    return x;
+}
+
+int jugador::getY() {
+    return y;
+}
+
+void jugador::mover(int dx, int dy) {
+    x += dx;
+    y += dy;
+}
