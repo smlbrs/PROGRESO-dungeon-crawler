61ca7fe0eb9b67bcb1dd2970e5837be251538f64
Samuel Barbosa Morales <zzzdy11@gmail.com>
2026-05-13 18:03:40 -0500

    Definir el inventario

---
+++ ProyectoDungeon/include/inventario.h
@@ -0,0 +1,20 @@
+#ifndef INVENTARIO_H
+#define INVENTARIO_H
+
+#include <string>
+
+class Inventario {
+private:
+    std::string objetos[5];
+    int cantidad;
+
+public:
+    Inventario();
+
+    void agregarObjeto(std::string objeto);
+
+    void mostrar();
+
+};
+
+#endif
