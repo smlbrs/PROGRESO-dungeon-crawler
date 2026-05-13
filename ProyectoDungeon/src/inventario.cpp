766c1e827e63704b3923ed6df1a33c3de76e780a
Samuel Barbosa Morales <zzzdy11@gmail.com>
2026-05-13 18:04:06 -0500

    implementacion el inventario

---
+++ ProyectoDungeon/src/inventario.cpp
@@ -0,0 +1,26 @@
+#include "../include/inventario.h"
+#include <iostream>
+
+Inventario::Inventario() {
+
+    cantidad = 0;
+}
+
+void Inventario::agregarObjeto(std::string objeto) {
+
+    if(cantidad < 5) {
+
+        objetos[cantidad] = objeto;
+        cantidad++;
+    }
+}
+
+void Inventario::mostrar() {
+
+    std::cout << "\nInventario:\n";
+
+    for(int i = 0; i < cantidad; i++) {
+
+        std::cout << "- " << objetos[i] << std::endl;
+    }
+}
